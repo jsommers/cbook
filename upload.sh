d9ccf8dbc5459bb079e497ff50c20652670f7c59
Joel Sommers <jsommers@colgate.edu>
2015-07-21 21:09:58 -0400

    Fix upload directory

--- upload.sh
+++ upload.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -x
 
-scp -r _build/html jsommers@cs.colgate.edu:~/public_html/cosc301/book
-scp _build/xetex/cbook301.pdf jsommers@cs.colgate.edu:~/public_html/cosc301/book
+scp -r _build/html jsommers@cs.colgate.edu:~/public_html/cosc301/cbook
+scp _build/xetex/cbook301.pdf jsommers@cs.colgate.edu:~/public_html/cosc301/cbook
