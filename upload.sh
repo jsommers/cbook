ae1b3dee0a4980c566b1fa9db23535bc35428f6b
Joel Sommers <jsommers@colgate.edu>
2017-01-25 15:21:27 +1300

    add epub as upload target

--- upload.sh
+++ upload.sh
@@ -2,3 +2,4 @@
 
 scp -r _build/html jsommers@cs.colgate.edu:~/public_html/cosc301/cbook
 scp _build/xetex/cbook301.pdf jsommers@cs.colgate.edu:~/public_html/cosc301/cbook
+scp _build/epub/TheCOSC301BookofC.epub jsommers@cs.colgate.edu:~/public_html/cosc301/cbook/cbook301.epub
