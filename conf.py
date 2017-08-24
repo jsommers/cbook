d6cbac3c3ff1a7710340b941f34fa5253b6cbe7d
Joel Sommers <jsommers@colgate.edu>
2017-08-24 13:48:19 -0400

    Fix a couple typos; bump version number

--- conf.py
+++ conf.py
@@ -16,7 +16,7 @@
 import sys
 import os
 
-version = '2017.01'
+version = '2017.08'
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
