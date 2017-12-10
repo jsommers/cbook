3ca6dda755bc62baaf0a8dc93e3d87a020b84b1f
Joel Sommers <jsommers@colgate.edu>
2017-12-10 12:59:08 -0500

    remove obsolete upload script

--- upload.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -x
-
-scp -r _build/html jsommers@cs.colgate.edu:~/public_html/cbook
-scp _build/xetex/cbook301.pdf jsommers@cs.colgate.edu:~/public_html/cbook
-scp index.html jsommers@cs.colgate.edu:~/public_html/cbook
